51cfd32b4cb7cf4f94f11406c12c787bc1e13447
shivam-goswami5123 <shivam.goswami.ug22@nsut.ac.in>
2025-01-04 23:50:34 +0530

    refactor: update Dockerfiles to improve build context and structure; add initialization SQL script; enhance config validation

--- shared/config.js
+++ shared/config.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 
+if (!process.env.DB_USER || !process.env.DB_PASSWORD) {
+    throw new Error('Missing required environment variables');
+}
+
 module.exports = {
-    DB_HOST: process.env.DB_HOST || "localhost",
-    DB_PORT: process.env.DB_PORT || 5432,
-    DB_NAME: process.env.DB_NAME || "blog-web-app" ,
-    DB_USER: process.env.DB_USER || "postgres",
-    DB_PASSWORD: process.env.DB_PASSWORD || "PaRRot&8426" ,
-    JWT_SECRET: process.env.JWT_SECRET || "iamdevlopingcodingblocksproject"
+    DB_HOST: process.env.DB_HOST ,
+    DB_PORT: process.env.DB_PORT,
+    DB_NAME: process.env.DB_NAME ,
+    DB_USER: process.env.DB_USER ,
+    DB_PASSWORD: process.env.DB_PASSWORD ,
+    JWT_SECRET: process.env.JWT_SECRET 
 };
